247e1c871623a222544ab3cccabda5451831a514
kshitij <Kshitijnegi@gnail.com>
2020-04-17 19:23:03 +0530

    Initial Commit

---
+++ RatingsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingsApp
+//
+//  Created by Kshitij Negi on 17/04/20.
+//  Copyright © 2020 Kshitij Negi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
